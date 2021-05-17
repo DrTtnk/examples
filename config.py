4f349055896cebbfef733cffdd04e061920197bc
pietro <pietro@coolshop.it>
2021-05-17 12:16:00 +0200

    [CLUSTER] sadafa

--- config.py
+++ config.py
@@ -25,4 +25,9 @@ experiment_buddy.register(locals())
 learning_rate = jax.experimental.optimizers.inverse_time_decay(initial_lr, decay_steps, decay_factor, staircase=True)
 eval_every = math.ceil(num_epochs / 1000)
 
-tensorboard = experiment_buddy.deploy(host="", sweep_yaml="")
+RUNNER = {
+    "local": "",
+    "cluster": "mila"
+}
+
+tensorboard = experiment_buddy.deploy(host=RUNNER['cluster'], sweep_yaml="")
